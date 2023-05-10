6a07d8c08c81949c7bc59429314a4b7a19d22f81
Hermes785 <hermesnguefack785@gmail.com>
2023-05-10 21:38:56 +0200

    update version

--- backend/Controller/Formation.controller.js
+++ backend/Controller/Formation.controller.js
@@ -1,4 +1,4 @@
-const FormationModel = require('../models/formation.model');
+const FormationModel = require('../Models/formation.model');
 
 module.exports.getFormation = async (req, res) => {
     const formation = await FormationModel.find();
