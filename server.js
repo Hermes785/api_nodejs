8fe7dc9490d9b6a873dde88c914ba9dffc7e159f
Hermes785 <hermesnguefack785@gmail.com>
2023-05-15 19:19:46 +0200

    update versionss

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://degree-release.s3-website-eu-west-1.amazonaws.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(express.json());
 
 app.use(express.urlencoded({extended:false}));
