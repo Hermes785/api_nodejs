2fc3ae6f6b39c8c6f37300510be6d00db509f3bb
Hermes785 <hermesnguefack785@gmail.com>
2023-05-17 11:37:41 +0200

    update branche

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const dotenv = require("dotenv").config();
+const LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
+const session = require('express-session');
+const passport = require('passport');
+const app = express();
+const cors =require('cors')
+const port= process.env.PORT;
+//rend le .env connu dans tous le projet
+app.use(cors({ origin: ['http://localhost:3000', 'http://degree-release.s3-website-eu-west-1.amazonaws.com'] }));
+
+const connectDB = require('./backend/config/db.js');
+const { loginWithLinkedInCallback } = require('./backend/Controller/AuthentificationLinkdin.controller.js');
+
+app.use(express.json());
+
+app.use(express.urlencoded({extended:false}));
+
+connectDB();
+// Configuration de la session
+const sessionConfig = {
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  };
+// Ajout du middleware session
+app.use(session(sessionConfig));
+
+// Initialisation de Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use("/formation",require("./backend/Routes/Formation.route.js"));
+
+app.use("/auth",require("./backend/Routes/AuthentificationLinkdin.route.js"));
+
+app.use("/auth",require("./backend/Routes/AuthentificationGoogle.route.js"));
+
+app.use("/auth",require("./backend/Routes/AuthentificationFacebook.route.js"));
+
+app.use("/register", require("./backend/Routes/User.route.js"))
+
+app.use('/login',require("./backend/Routes/Login.route.js"))
+
+app.listen(port,()=> console.log('le server a demarer sur le port'+" "+port));
